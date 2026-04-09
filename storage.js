a800409fc678743571ead3318bc3acf56634dfe2
ma13vq8-gif <ma13vq8@gmail.com>
2026-04-09 22:49:14 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,6 +1,6 @@
 // SKYNX STORAGE - All data in one file
 const STORAGE = {
-  users: [{"u":"Skynx","p":".Ninjistro","age":13,"name":"Skynx_Official"}],
+  users: [],
   messages: [],
   friends: { friendships: {} },
   admins: {
